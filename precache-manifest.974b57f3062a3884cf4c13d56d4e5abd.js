c946df978119ce1a12c1c20948f0fa285aa88a0c
marmon179 <marmon179@gmail.com>
2021-05-18 15:57:43 +0300

    Updates

---
+++ precache-manifest.974b57f3062a3884cf4c13d56d4e5abd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9380077670f0ad2900c99d756177ae41",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "f4d752221e1c922ea95b",
+    "url": "/Todolist/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "aa48b4dbe1e8526106e3",
+    "url": "/Todolist/static/js/2.5b768445.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/Todolist/static/js/2.5b768445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d752221e1c922ea95b",
+    "url": "/Todolist/static/js/main.f33d176e.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
